d9d51fef9ba63a67ec4b7244ba25789685d344db
XiaoruiZhu <zhuxiaorui1989@gmail.com>
2016-03-30 11:51:52 -0400

    Revised for wrong file format, missing file name, and merging variable missing.

--- R/MergerXLSX.R
+++ R/MergerXLSX.R
@@ -11,20 +11,29 @@
 ##' @importFrom xlsx read.xlsx2
 ##' @return Return data are all original data with addin variables.
 ##' @author Xiaorui (Jeremy) Zhu
-##' @export
+##' @export tools file_ext
 ##' @examples
-##' # file1 <- "C:/data.xlsx"
-##' # file2 <- "C:/data2.xlsx"
-##' # merged <- MergerXLSX(file1, file2, mergeID)
+##' file1 <- system.file("tests", "test_import1.xlsx", package = "DataClean")
+##' file2 <- system.file("tests", "test_import2.xlsx", package = "DataClean")
+##' # merge two xlsx files by id
+##' Merged <- MergerXLSX(original_file=file1, addin_file=file2, mergeID = "user_id)
+##' head(Merged)
 MergerXLSX <- function(original_file, addin_file, mergeID){
   if (missing(original_file) | missing(addin_file)) {
     stop("Two 'files' must be a character string of name if it is saved in working directory, or it should include saving path of file.")
   }
-
+  if (file_ext(original_file) != "xlsx" | file_ext(addin_file) != "xlsx")
+  {
+    stop("Please check the format of files. They should be 'xlsx' file. If not, change them then try again.")
+  }
   original <- read.xlsx2(original_file, 1)
   addin <- read.xlsx2(addin_file, 1)
-  sub.list <- split(original, rownames(original))
-  results <- lapply(sub.list, consolida, data = addin, mergeVar = mergeID)
-  combined <- do.call(rbind.data.frame, results)
-  return(combined)
+  if (sum(names(original)==mergeID)==0){
+    stop("No same column name in either file. Please double check same ID name appears in both xlsx files.")
+  } else {
+    sub.list <- split(original, rownames(original))
+    results <- lapply(sub.list, consolida, data = addin, mergeVar = mergeID)
+    combined <- do.call(rbind.data.frame, results)
+    return(combined)
+  }
   }
